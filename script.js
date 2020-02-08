a84d5142381dd6085e8d15ebe403c6201988f40c
‘Juliano <julianoperins@gmail.com>
2020-02-08 01:15:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.getElementbyId('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime/ 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+    
+}
